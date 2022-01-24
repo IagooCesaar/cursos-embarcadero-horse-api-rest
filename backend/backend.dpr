dfce58397f442d0279543efbb45e8ae8e80d25f7
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2022-01-24 08:48:15 -0300

    Rota para deletar usuário

--- backend/backend.dpr
+++ backend/backend.dpr
@@ -39,5 +39,17 @@ begin
         .Send<TJSONAncestor>(user.Clone);
     end);
 
+  App.Delete('/users/:id',
+    procedure (Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var id: Integer;
+    begin
+      id  := Req.Params.Items['id'].ToInteger;
+      Users.Remove(id).Free;// id utilizado como índice
+
+      Res
+        .Status(THTTPStatus.NoContent)
+        .Send<TJSONAncestor>(Users.Clone);
+    end);
+
   App.Start;
 end.
