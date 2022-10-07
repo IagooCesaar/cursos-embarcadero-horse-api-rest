6a1e71398a86c22bdfc2ce5931b72a4ed390fc06
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2022-10-07 13:13:11 -0300

    novas units

--- relatorios/relatorios_horse.dpr
+++ relatorios/relatorios_horse.dpr
@@ -9,12 +9,14 @@ uses
   App in 'src\App.pas',
   Service.Base in 'src\services\Service.Base.pas' {ServiceBase: TDataModule},
   Service.Base.Simples in 'src\services\Service.Base.Simples.pas' {ServiceBaseSimples: TDataModule},
-  Service.Cliente in 'src\services\Service.Cliente.pas' {ServiceBaseCliente: TDataModule},
+  Service.Cliente in 'src\services\Service.Cliente.pas' {ServiceCliente: TDataModule},
   Reports.Base in 'src\reports\Reports.Base.pas' {ReportBase: TFrame},
   Reports.Base.Simples in 'src\reports\Reports.Base.Simples.pas' {ReportBaseSimples: TFrame},
-  Reports.Cliente in 'src\reports\Reports.Cliente.pas' {ReportBaseSimples1: TFrame};
+  Reports.Cliente in 'src\reports\Reports.Cliente.pas' {ReportCliente: TFrame},
+  Controllers.Cliente in 'src\controllers\Controllers.Cliente.pas';
 
 begin
+
   try
     App.Start;
   except
