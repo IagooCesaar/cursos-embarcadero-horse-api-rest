a310351ef57da67e58c9264dc36b9ce5e139ea4d
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2022-10-07 09:19:34 -0300

    Report.Base

--- relatorios/relatorios_horse.dpr
+++ relatorios/relatorios_horse.dpr
@@ -9,7 +9,8 @@ uses
   App in 'src\App.pas',
   Service.Base in 'src\services\Service.Base.pas' {ServiceBase: TDataModule},
   Service.Base.Simples in 'src\services\Service.Base.Simples.pas' {ServiceBaseSimples: TDataModule},
-  Service.Cliente in 'src\services\Service.Cliente.pas' {ServiceBaseCliente: TDataModule};
+  Service.Cliente in 'src\services\Service.Cliente.pas' {ServiceBaseCliente: TDataModule},
+  Report.Base in 'src\reports\Report.Base.pas' {Frame1: TFrame};
 
 begin
   try
---
+++ relatorios/src/reports/Report.Base.pas
@@ -0,0 +1,22 @@
+unit Report.Base;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, RLReport;
+
+type
+  TReportBase = class(TFrame)
+    Report: TRLReport;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+end.
